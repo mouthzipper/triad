536907b8f94295faec925e21dfad1e59e01b012f
Jerome Ramos <contact@ramosjero.me>
2024-03-09 10:59:08 +0800

    Login

--- hello_world.py
+++ hello_world.py
@@ -1,2 +1,16 @@
 
 print("hellow world1")
+def sum(a, b):
+    return a + b
+print("hellow world2")
+print(sum(1, 2))
+
+
+# Path: hello_world.py
+def login(username, password):
+    if username == "admin" and password == "admin":
+        return "Login Success"
+    else:
+        return "Login Failed"
+
+print(login("admin", "admin"))
